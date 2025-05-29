0bc0091878329f5b8316d00d8f1974e6f493526b
brendhacasaro <brendhameira9@gmail.com>
2025-05-28 21:34:05 -0300

    .

---
+++ semestre_2/cap7.c
@@ -0,0 +1,59 @@
+/*
+OBS.: Todos os programa devem ser finalizados pelo usuario.
+2 - Escreva um programa com a estrutura de dados abaixo. Receba 2 datas via
+    teclado na funcao main(). Faca uma funcao que calcule o numero de dias
+    entre elas e mostre o resultado no video na funcao main().
+    Utilize vetor de estruturas.
+                estrutura: dia, mes, ano
+
+3 - Escreva um programa que tem uma estrutura da dados com os membros abaixo.
+    Receba dados via teclado em uma funcao e imprima estes conteudos no video,
+    em outra funcao, no seguinte formato.
+     estrutura: char, int, long, float, double, unsigned char, unsigned int,
+                unsigned long
+
+            10        20        30        40        50        60        70
+    1234567890123456789012345678901234567890123456789012345678901234567890
+        char      int       long                float               double
+              unsigned char       unsigned int        unsigned long
+
+4 - Escreva um programa com a estrutura de dados abaixo. Defina um vetor de
+    estrutura de 4 elementos. Receba dados via teclado e imprima-os no video.
+    Faca um menu com as seguintes opcoes:
+    1 - receber todos os dados
+    2 - imprime todos os dados
+    3 - calcula o IMC de todas as pessoas.
+    4 - sair
+        Calculo do IMC = peso/(altura*altura).
+        estrutura: nome, peso, altura
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+#define ex1
+
+#ifdef ex1
+/*
+1 - Escreva um programa com a estrutura de dados abaixo. Defina um vetor de
+    estruturas de 4 elementos. Receba os dados pelo teclado em uma funcao
+    e imprima-os no video em uma funcao. Utilize vetor de estruturas.
+    Faca um menu com uma opcao para saida do programa.
+                estrutura: nome, end, cidade, estado, cep
+*/
+
+void dadosPessoa(Pessoa p) {}
+
+void main() {
+  typedef struct {
+    char nome[50];
+    char end[50];
+    char cidade[50];
+    char estado[50];
+    char cep[50];
+  } Pessoa;
+
+  Pessoa pessoas[4];
+}
+
+#endif // ex1
